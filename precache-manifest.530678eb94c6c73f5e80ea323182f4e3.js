868ea45fedfea08685934619fbe81f7fcf913a65
Jessie Sidhu <jsidhu22@gmail.com>
2019-08-08 12:34:44 -0700

    Updates

---
+++ precache-manifest.530678eb94c6c73f5e80ea323182f4e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975df25cda67ab7fa84faeeeb3c785bf",
+    "url": "/tip/index.html"
+  },
+  {
+    "revision": "679e2c090e750a7ccfee",
+    "url": "/tip/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7d77bcd887114083ce5a",
+    "url": "/tip/static/css/main.fae9e24b.chunk.css"
+  },
+  {
+    "revision": "679e2c090e750a7ccfee",
+    "url": "/tip/static/js/2.eb2dbdfe.chunk.js"
+  },
+  {
+    "revision": "7d77bcd887114083ce5a",
+    "url": "/tip/static/js/main.d34ebff6.chunk.js"
+  },
+  {
+    "revision": "1b573f72b09086f9eced",
+    "url": "/tip/static/js/runtime~main.a74dde57.js"
+  }
+]);
