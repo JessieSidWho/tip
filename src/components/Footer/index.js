f2c8b88a6e52672963cc5300730f092062ce4229
Jessie Sidhu <jsidhu22@gmail.com>
2019-08-08 14:26:52 -0700

    still debating footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import './module.css';
+
+
+const Title = () => {
+    return (
+        <>
+            <Navbar id='Navbar' fixed='bottom' className='row' expand="lg" bg="dark" variant="dark">
+                <div className='col-md-12'>
+
+                </div>
+            </Navbar>
+        </>
+    )
+}
+
+export default Title;
